cea286523bb70c4ba51b1347e9f29fdffc69b584
antmdev <executivesales@hotmail.co.uk>
2019-02-05 22:38:12 +0000

    Polynomial exercise Complete

--- 3_Polynomial_Linear_Regression/polynomial_regression_ant.py
+++ 3_Polynomial_Linear_Regression/polynomial_regression_ant.py
@@ -1,66 +1,77 @@
-# Polynomial Regression
+# Data Preprocessing Template
 
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+"""
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:2].values
+"""
+dataset = pd.read_csv('Position_Salaries.csv')]
+#using 1:2 means that X is now considered as a matrix rather than a vector
+#I>E MAtrix (10,0) and Y is a vector (10) just 10 rows.
+X = dataset.iloc[:, 1:2].values 
 y = dataset.iloc[:, 2].values
 
+"""
 # Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
-
+"""
+#No need to split into trainng and test ast there's only 10 values so theres not enough
+#data to properly train and test.
+#Also we need to make an extremely accurate prediction because we're tying to 
+#Predict where the employees salary actually sits... so we want to use all of the data.
+"""
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)"""
-
-# Fitting Linear Regression to the dataset
+"""
+#No need for Feature scaling as we're using the same linear regressoin library that
+#alredy includes feature scaling
+"""
+# Build the Linear Regression Model #so we can compare to PLR
+"""
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
-
-# Fitting Polynomial Regression to the dataset
+"""
+# Build the Polynomial Linear Regression Model #so we can compare to PLR
+"""
+#create the X_poly matrix first
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 4)
-X_poly = poly_reg.fit_transform(X)
-poly_reg.fit(X_poly, y)
+#Transform X matrix of features containig only the position levels in to the X_poly  
+X_poly = poly_reg.fit_transform(X) 
+#Then create a second regression and fit to the new X_poly Set
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
-# Visualising the Linear Regression results
-plt.scatter(X, y, color = 'red')
+"""
+# Visualise the Linear Regression result
+"""
+plt.scatter(X, y, color = 'red') #Base results Level Vs Salary from Company
 plt.plot(X, lin_reg.predict(X), color = 'blue')
 plt.title('Truth or Bluff (Linear Regression)')
-plt.xlabel('Position level')
+plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
 
-# Visualising the Polynomial Regression results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
-
-# Visualising the Polynomial Regression results (for higher resolution and smoother curve)
+"""
+# Visualise the Polynomial Linear Regression result
+"""
 X_grid = np.arange(min(X), max(X), 0.1)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
-plt.xlabel('Position level')
+X_grid = X_grid.reshape((len(X_grid), 1))#Make a matrix of 0.1 increments 90 columns improve accuracy
+plt.scatter(X, y, color = 'red') #Base results Level Vs Salary from Company
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
 
-# Predicting a new result with Linear Regression
-lin_reg.predict(6.5)
 
-# Predicting a new result with Polynomial Regression
-lin_reg_2.predict(poly_reg.fit_transform(6.5))
+
+
+
+
+
+
+
+
